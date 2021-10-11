5a69af3f056e10f29fa76b4a6ff49fb5aa5e921b
longnd2 <longnd2@rikkeisoft.com>
2021-10-11 20:54:11 +0700

    Updates

---
+++ precache-manifest.b6ea0e8df1e82b11d0aef212ff4c1454.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "f64e162408347e5acc70",
+    "url": "/admin1999store/static/css/main.4fb6304f.chunk.css"
+  },
+  {
+    "revision": "f64e162408347e5acc70",
+    "url": "/admin1999store/static/js/main.f64e1624.chunk.js"
+  },
+  {
+    "revision": "5ec27ccb76894869b34d",
+    "url": "/admin1999store/static/css/1.3e3a0883.chunk.css"
+  },
+  {
+    "revision": "5ec27ccb76894869b34d",
+    "url": "/admin1999store/static/js/1.5ec27ccb.chunk.js"
+  },
+  {
+    "revision": "9a238362fa59bde6a8fc",
+    "url": "/admin1999store/static/js/runtime~main.9a238362.js"
+  },
+  {
+    "revision": "f47e9b358bf412ea61bc571f277a11dc",
+    "url": "/admin1999store/static/media/logo.f47e9b35.svg"
+  },
+  {
+    "revision": "21743d6e0f2dca87f2ddb53d1bf82ce2",
+    "url": "/admin1999store/static/media/login-form.21743d6e.svg"
+  },
+  {
+    "revision": "1befd1b556c23c9fdfaf0a2acf4ad2d1",
+    "url": "/admin1999store/static/media/login-bg.1befd1b5.svg"
+  },
+  {
+    "revision": "6e897eaa71d6a68fe5420f98dd1fd104",
+    "url": "/admin1999store/static/media/login-person.6e897eaa.svg"
+  },
+  {
+    "revision": "825e873f928a8845dcc4eef0f7d3606c",
+    "url": "/admin1999store/static/media/login-screen1.825e873f.svg"
+  },
+  {
+    "revision": "b829ff392ec65818536520c1706667d4",
+    "url": "/admin1999store/static/media/login-screen2.b829ff39.svg"
+  },
+  {
+    "revision": "d1c9ffa74d7189502e2ef8dea1e0203b",
+    "url": "/admin1999store/static/media/login-screen3.d1c9ffa7.svg"
+  },
+  {
+    "revision": "47cd0aefda75e2fb1619debf2ffa20c4",
+    "url": "/admin1999store/static/media/login-icons.47cd0aef.svg"
+  },
+  {
+    "revision": "6a9ae17faef829868266ddbd3b71f35f",
+    "url": "/admin1999store/static/media/fontello.6a9ae17f.eot"
+  },
+  {
+    "revision": "e36f163c9dcb27520fe14555c9006496",
+    "url": "/admin1999store/static/media/fontello.e36f163c.woff"
+  },
+  {
+    "revision": "993ba0f4be446a96fab6282a70724461",
+    "url": "/admin1999store/static/media/fontello.993ba0f4.ttf"
+  },
+  {
+    "revision": "c558aff90b517bfa8af5063b96698106",
+    "url": "/admin1999store/static/media/fontello.c558aff9.svg"
+  },
+  {
+    "revision": "d340718781c2a5cd05d0b0436f5796d3",
+    "url": "/admin1999store/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/admin-ecommerce/precache-manifest.c91205c42b262b4843b3bb0cb6418fe4.js"
+  "/admin1999store/precache-manifest.b6ea0e8df1e82b11d0aef212ff4c1454.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/admin-ecommerce/index.html", {
+workbox.routing.registerNavigationRoute("/admin1999store/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
