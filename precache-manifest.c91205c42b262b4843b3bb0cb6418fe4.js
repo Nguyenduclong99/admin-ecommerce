65dfda936c60a0b02621db3c7aeff1ba35ced01f
longnd2 <longnd2@rikkeisoft.com>
2021-10-11 19:48:02 +0700

    Updates

---
+++ precache-manifest.c91205c42b262b4843b3bb0cb6418fe4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab46af7d25735b3b1f2d",
+    "url": "/admin-ecommerce/static/css/main.90938aeb.chunk.css"
+  },
+  {
+    "revision": "ab46af7d25735b3b1f2d",
+    "url": "/admin-ecommerce/static/js/main.ab46af7d.chunk.js"
+  },
+  {
+    "revision": "5ec27ccb76894869b34d",
+    "url": "/admin-ecommerce/static/css/1.3e3a0883.chunk.css"
+  },
+  {
+    "revision": "5ec27ccb76894869b34d",
+    "url": "/admin-ecommerce/static/js/1.5ec27ccb.chunk.js"
+  },
+  {
+    "revision": "013937fb77e456fea5bc",
+    "url": "/admin-ecommerce/static/js/runtime~main.013937fb.js"
+  },
+  {
+    "revision": "f47e9b358bf412ea61bc571f277a11dc",
+    "url": "/admin-ecommerce/static/media/logo.f47e9b35.svg"
+  },
+  {
+    "revision": "21743d6e0f2dca87f2ddb53d1bf82ce2",
+    "url": "/admin-ecommerce/static/media/login-form.21743d6e.svg"
+  },
+  {
+    "revision": "1befd1b556c23c9fdfaf0a2acf4ad2d1",
+    "url": "/admin-ecommerce/static/media/login-bg.1befd1b5.svg"
+  },
+  {
+    "revision": "6e897eaa71d6a68fe5420f98dd1fd104",
+    "url": "/admin-ecommerce/static/media/login-person.6e897eaa.svg"
+  },
+  {
+    "revision": "825e873f928a8845dcc4eef0f7d3606c",
+    "url": "/admin-ecommerce/static/media/login-screen1.825e873f.svg"
+  },
+  {
+    "revision": "b829ff392ec65818536520c1706667d4",
+    "url": "/admin-ecommerce/static/media/login-screen2.b829ff39.svg"
+  },
+  {
+    "revision": "d1c9ffa74d7189502e2ef8dea1e0203b",
+    "url": "/admin-ecommerce/static/media/login-screen3.d1c9ffa7.svg"
+  },
+  {
+    "revision": "47cd0aefda75e2fb1619debf2ffa20c4",
+    "url": "/admin-ecommerce/static/media/login-icons.47cd0aef.svg"
+  },
+  {
+    "revision": "6a9ae17faef829868266ddbd3b71f35f",
+    "url": "/admin-ecommerce/static/media/fontello.6a9ae17f.eot"
+  },
+  {
+    "revision": "e36f163c9dcb27520fe14555c9006496",
+    "url": "/admin-ecommerce/static/media/fontello.e36f163c.woff"
+  },
+  {
+    "revision": "993ba0f4be446a96fab6282a70724461",
+    "url": "/admin-ecommerce/static/media/fontello.993ba0f4.ttf"
+  },
+  {
+    "revision": "c558aff90b517bfa8af5063b96698106",
+    "url": "/admin-ecommerce/static/media/fontello.c558aff9.svg"
+  },
+  {
+    "revision": "6fac5e6f9f625a0a5034a6656cb137ff",
+    "url": "/admin-ecommerce/index.html"
+  }
+];
